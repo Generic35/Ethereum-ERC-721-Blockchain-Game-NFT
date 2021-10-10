8c354af68b10e34ef325c803d6e1ceae57863dc0
Eric Lamontagne <ricklamontagne@gmail.com>
2021-10-10 18:33:01 -0400

    build: Deploy the memory token

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,16 @@
+/* eslint-disable */
 import React, { Component } from 'react';
-import Web3 from 'web3'
+import Web3 from 'web3';
 import './App.css';
-import MemoryToken from '../abis/MemoryToken.json'
-import brain from '../brain.png'
+import MemoryToken from '../abis/MemoryToken.json';
+import brain from '../brain.png';
 
 class App extends Component {
-
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      account: '0x0'
-    }
+      account: '0x0',
+    };
   }
 
   render() {
@@ -24,12 +23,20 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-          <img src={brain} width="30" height="30" className="d-inline-block align-top" alt="" />
-          &nbsp; Memory Tokens
+            <img
+              src={brain}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt=""
+            />
+            &nbsp; Memory Tokens
           </a>
           <ul className="navbar-nav px-3">
             <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-              <small className="text-muted"><span id="account">{this.state.account}</span></small>
+              <small className="text-muted">
+                <span id="account">{this.state.account}</span>
+              </small>
             </li>
           </ul>
         </nav>
@@ -39,26 +46,14 @@ class App extends Component {
               <div className="content mr-auto ml-auto">
                 <h1 className="d-4">Edit this file in App.js!</h1>
 
-                <div className="grid mb-4" >
-
-                  {/* Code goes here... */}
-
-                </div>
+                <div className="grid mb-4">{/* Code goes here... */}</div>
 
                 <div>
-
                   {/* Code goes here... */}
 
-                  <div className="grid mb-4" >
-
-                    {/* Code goes here... */}
-
-                  </div>
-
+                  <div className="grid mb-4">{/* Code goes here... */}</div>
                 </div>
-
               </div>
-
             </main>
           </div>
         </div>
