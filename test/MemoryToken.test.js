6b8abc19f865e9342fadcb3cc29b1b2e6a63cdb7
Eric Lamontagne <ricklamontagne@gmail.com>
2021-10-10 18:52:41 -0400

    test: Token should have name and symbol

--- test/MemoryToken.test.js
+++ test/MemoryToken.test.js
@@ -7,14 +7,26 @@ require('chai')
 contract('Memory Token', (accounts) => {
   let token;
 
+  before(async () => {
+    token = await MemoryToken.deployed();
+  });
   describe('deployment', async () => {
     it('deploys successfully', async () => {
-      token = await MemoryToken.deployed();
       const address = token.address;
       assert.notEqual(address, 0x0);
       assert.notEqual(address, '');
       assert.notEqual(address, null);
       assert.notEqual(address, undefined);
+    });
+
+    it('has a name', async () => {
+      const name = await token.name();
+      assert.equal(name, 'Memory Token');
+    });
+
+    it('has a symbol', async () => {
+      const symbol = await token.symbol();
+      assert.equal(symbol, 'MEMORY');
     });
   });
 });
