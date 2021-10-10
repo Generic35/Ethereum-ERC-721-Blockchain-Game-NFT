57b372e86f78ea7aee62625cb170df16789896d2
Eric Lamontagne <ricklamontagne@gmail.com>
2021-10-10 19:09:24 -0400

    feat: Add ability to mint, and test it should mint

--- src/contracts/MemoryToken.sol
+++ src/contracts/MemoryToken.sol
@@ -6,6 +6,12 @@ contract MemoryToken is ERC721Full{
 	string public name = "Memory Token";
 
 	constructor() ERC721Full("Memory Token", "MEMORY") public {
-		
+	}
+
+	function mint(address _to, string memory _tokenURI) public returns(bool) {
+		uint _tokenId = totalSupply().add(1);
+		_mint(_to, _tokenId);
+		_setTokenURI(_tokenId, _tokenURI);
+		return true;
 	}
 }
