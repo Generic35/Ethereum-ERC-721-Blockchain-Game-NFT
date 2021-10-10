e1ad0b7acde44bf372d0ad6bac219faf111d8cfe
Eric Lamontagne <ricklamontagne@gmail.com>
2021-10-10 18:48:53 -0400

    test: Should deploy contract

--- src/contracts/MemoryToken.sol
+++ src/contracts/MemoryToken.sol
@@ -2,7 +2,10 @@ pragma solidity ^0.5.0;
 
 import "./ERC721Full.sol";
 
-contract MemoryToken {
+contract MemoryToken is ERC721Full{
 	string public name = "Memory Token";
-	
+
+	constructor() ERC721Full("Memory Token", "MEMORY") public {
+		
+	}
 }
--- test/MemoryToken.test.js
+++ test/MemoryToken.test.js
@@ -1,9 +1,20 @@
-const MemoryToken = artifacts.require('./MemoryToken.sol')
+const MemoryToken = artifacts.require('./MemoryToken.sol');
 
 require('chai')
   .use(require('chai-as-promised'))
-  .should()
+  .should();
 
 contract('Memory Token', (accounts) => {
-  // code goes here...
-})
+  let token;
+
+  describe('deployment', async () => {
+    it('deploys successfully', async () => {
+      token = await MemoryToken.deployed();
+      const address = token.address;
+      assert.notEqual(address, 0x0);
+      assert.notEqual(address, '');
+      assert.notEqual(address, null);
+      assert.notEqual(address, undefined);
+    });
+  });
+});
